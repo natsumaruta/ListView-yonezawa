15f23f15b7b33c48008a57576a41eb66e4f0ba08
natsumaruta <nabeta0915@gmail.com>
2025-02-09 14:34:34 +0900

    Initial Commit

---
+++ ListView/ListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewApp.swift
+//  ListView
+//
+//  Created by 米澤菜摘子 on 2025/02/09.
+//
+
+import SwiftUI
+
+@main
+struct ListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
